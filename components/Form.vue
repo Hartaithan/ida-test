a8818984f8124cbee95328b7f4d7b5bdfa04ba9c
hartaithan <hartaithan@gmail.com>
2022-02-17 15:14:21 +0900

    form validation

--- components/Form.vue
+++ components/Form.vue
@@ -1,29 +1,70 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit()">
     <div class="input-group">
-      <label for="name">Наименование товара</label>
+      <label class="input-group__label" for="name">Наименование товара</label>
       <input
         id="name"
+        v-model="form.name"
+        class="input-group__input"
+        :class="{ invalid: errors.name }"
         type="text"
         placeholder="Введите наименование товара"
-        required
+        @change="validateInput('name')"
       >
+      <div v-if="errors.name" class="input-group__feedback">
+        {{ errors.name }}
+      </div>
     </div>
     <div class="input-group">
-      <label for="description">Описание товара</label>
+      <label
+        class="input-group__label"
+        for="description"
+      >Описание товара</label>
       <textarea
         id="description"
+        v-model="form.description"
+        class="input-group__textarea"
+        :class="{ invalid: errors.description }"
         type="text"
         placeholder="Введите описание товара"
+        @change="validateInput('description')"
       />
+      <div v-if="errors.description" class="input-group__feedback">
+        {{ errors.description }}
+      </div>
     </div>
     <div class="input-group">
-      <label for="link">Ссылка на изображение товара</label>
-      <input id="link" type="text" placeholder="Введите ссылку" required>
+      <label
+        class="input-group__label"
+        for="link"
+      >Ссылка на изображение товара</label>
+      <input
+        id="link"
+        v-model="form.link"
+        class="input-group__input"
+        :class="{ invalid: errors.link }"
+        type="text"
+        placeholder="Введите ссылку"
+        @change="validateInput('link')"
+      >
+      <div v-if="errors.link" class="input-group__feedback">
+        {{ errors.link }}
+      </div>
     </div>
     <div class="input-group">
-      <label for="price">Цена товара</label>
-      <input id="price" type="number" placeholder="Введите цену" required>
+      <label class="input-group__label" for="price">Цена товара</label>
+      <input
+        id="price"
+        v-model="form.price"
+        class="input-group__input"
+        :class="{ invalid: errors.price }"
+        type="number"
+        placeholder="Введите цену"
+        @change="validateInput('price')"
+      >
+      <div v-if="errors.price" class="input-group__feedback">
+        {{ errors.price }}
+      </div>
     </div>
     <button type="submit">
       Добавить товар
@@ -33,7 +74,51 @@
 
 <script>
 export default {
-  name: 'FormComponent'
+  name: 'FormComponent',
+  data () {
+    return {
+      form: {
+        name: '',
+        description: '',
+        link: '',
+        price: null
+      },
+      errors: {}
+    }
+  },
+  methods: {
+    handleSubmit () {
+      if (this.validateForm()) {
+        alert(JSON.stringify(this.form))
+      }
+    },
+    validateForm () {
+      const errors = {}
+      if (this.form.name.length === 0) {
+        errors.name = "Поле 'Наименование товара' обязательно для заполнения"
+      }
+      if (this.form.description.length === 0) {
+        errors.description =
+          "Поле 'Описание товара' обязательно для заполнения"
+      }
+      if (this.form.link.length === 0) {
+        errors.link =
+          "Поле 'Ссылка на изображение товара' обязательно для заполнения"
+      }
+      if (!this.form.price) {
+        errors.price = "Поле 'Цена товара' обязательно для заполнения"
+      }
+      this.errors = errors
+      return errors.length === 0
+    },
+    validateInput (id) {
+      const errors = { ...this.errors }
+      if (this.form[id].length > 0) {
+        delete errors[id]
+        this.errors = errors
+      }
+    }
+  }
 }
 </script>
 
@@ -48,27 +133,41 @@ form {
 .input-group {
   display: flex;
   flex-direction: column;
-  label {
+  &__label {
     font-size: 10px;
     line-height: 13px;
     letter-spacing: -0.02em;
     color: #49485e;
     margin-bottom: 4px;
   }
-  input,
-  textarea {
+  &__input,
+  &__textarea {
     width: 100%;
     height: 36px;
     background: #fffefb;
     box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1);
-    border: none;
+    border: 1px solid transparent;
     border-radius: 4px;
     padding: 10px 16px;
+    &.invalid {
+      border: 1px solid #ff8484;
+    }
     &::placeholder {
       font-size: 12px;
       line-height: 15px;
       color: #b4b4b4;
     }
+  }
+  &__textarea {
+    min-height: 108px;
+    resize: none;
+  }
+  &__feedback {
+    margin-top: 4px;
+    font-size: 8px;
+    line-height: 10px;
+    letter-spacing: -0.02em;
+    color: #ff8484;
   }
   input {
     &::-webkit-outer-spin-button,
@@ -79,10 +178,6 @@ form {
     &[type="number"] {
       -moz-appearance: textfield;
     }
-  }
-  textarea {
-    min-height: 108px;
-    resize: none;
   }
   &:not(:last-child) {
     margin-bottom: 16px;
