d3f574d504329ed10be2a03e3b91f17511199f7e
hartaithan <hartaithan@gmail.com>
2022-06-23 21:12:16 +0900

    refactor sort logic

--- store/index.js
+++ store/index.js
@@ -2,14 +2,43 @@ export const state = () => ({
   list: [],
   unsorted: [],
   alertIsOpen: false,
-  sidebarIsOpen: false
+  sidebarIsOpen: false,
+  sortBy: null
 })
+
+const sort = (sortBy, list) => {
+  const items = [...list]
+  switch (sortBy) {
+    case 0:
+      return items.sort((a, b) =>
+        parseFloat(a.id) > parseFloat(b.id) ? 1 : -1
+      )
+    case 1:
+      return items.sort((a, b) =>
+        parseFloat(a.price.replace(' ', '')) >
+        parseFloat(b.price.replace(' ', ''))
+          ? 1
+          : -1
+      )
+    case 2:
+      return items.sort((a, b) =>
+        parseFloat(b.price.replace(' ', '')) >
+        parseFloat(a.price.replace(' ', ''))
+          ? 1
+          : -1
+      )
+    case 3:
+      return items.sort((a, b) => (a.name > b.name ? 1 : -1))
+    default:
+      break
+  }
+}
 
 export const mutations = {
   addItem (state, form) {
     const id =
       state.list.length > 0 ? state.list[state.list.length - 1].id + 1 : 1
-    state.list = [
+    const newList = [
       ...state.list,
       {
         id,
@@ -19,7 +48,12 @@ export const mutations = {
         price: form.price
       }
     ]
-    localStorage.setItem('list', JSON.stringify(state.list))
+    if (state.sortBy) {
+      state.list = sort(state.sortBy, newList)
+    } else {
+      state.list = newList
+    }
+    localStorage.setItem('list', JSON.stringify(newList))
   },
   deleteItem (state, id) {
     const items = [...state.list].filter(item => item.id !== id)
@@ -30,35 +64,23 @@ export const mutations = {
     state.list = list
   },
   sortBy (state, sortType) {
+    state.sortBy = sortType
     if (state.unsorted.length === 0) {
       state.unsorted = [...state.list]
     }
     const items = [...state.list]
     switch (sortType) {
       case 0:
-        state.list = [...state.unsorted]
+        state.list = sort(sortType, items)
         break
       case 1:
-        items.sort((a, b) =>
-          parseFloat(a.price.replace(' ', '')) >
-          parseFloat(b.price.replace(' ', ''))
-            ? 1
-            : -1
-        )
-        state.list = items
+        state.list = sort(sortType, items)
         break
       case 2:
-        items.sort((a, b) =>
-          parseFloat(b.price.replace(' ', '')) >
-          parseFloat(a.price.replace(' ', ''))
-            ? 1
-            : -1
-        )
-        state.list = items
+        state.list = sort(sortType, items)
         break
       case 3:
-        items.sort((a, b) => (a.name > b.name ? 1 : -1))
-        state.list = items
+        state.list = sort(sortType, items)
         break
       default:
         break
